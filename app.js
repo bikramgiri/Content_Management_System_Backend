7694838ae30a5d9c1500f827fe05e7eb07c1ffa8
bikramgiri <asgbrg.github@gmail.com>
2025-07-01 14:19:14 +0545

    develop create blog api and successfully test API in postman

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 require('dotenv').config()
-const app = require('express')()
+const express = require('express')
+const app = express()
 const mongoose = require('mongoose')
+const Blog = require('./model/blogModel')
+
+// Node js lai form bta aako data lai handle garna ko lagi
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Database connection
-const { connectDatabase } = require('./database/database')
+const { connectDatabase } = require('./database/database');
 connectDatabase();
 
 app.get('/',(req, res)=>{
       res.json({
-            message : "Welcome to the Content Management System Home Page",
-            status : "200"
+            status: 200,
+            message : "Welcome to the Content Management System",
       })
 })
+
+// create a Blog API
+app.post('/createBlog', async(req, res)=>{
+      const { title, subTitle, description } = req.body;
+
+      // Insert to database logic here
+      await Blog.create({
+            title : title,
+            subTitle : subTitle,
+            description : description
+      })
+
+      res.json({
+            status: 200,
+            message: "Blog created successfully"
+      })
+      // Or
+      // res.status(200).json({
+      //       message: "Blog created successfully"
+      // })
+})
+
 
 const port = process.env.PORT || 2000
 app.listen(port,()=>{
---
+++ model/blogModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: {      
+            type: String,
+            required: true
+      },
+      subTitle: {
+            type: String,
+            required: true
+      },
+      description: {
+            type: String,
+            required: true
+      }
+},{
+      timestamps: true
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
+
+
+// Or
+// module.exports = mongoose.model('Blog', blogSchema)
