b642d4de02ab34fbd6ebeec332bca7286d291fea
bikramgiri <asgbrg.github@gmail.com>
2025-07-02 13:55:46 +0545

    develop all read and single read blog api

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/',(req, res)=>{
       })
 })
 
-// create a Blog API
+// **create a Blog API
 app.post('/createBlog', async(req, res)=>{
       const { title, subTitle, description } = req.body;
 
@@ -40,6 +40,65 @@ app.post('/createBlog', async(req, res)=>{
       // })
 })
 
+// **Get all blogs API
+app.get("/blogs", async(req, res)=>{
+      // fetsch all blogs from Blog Model
+      const blogs = await Blog.find()
+
+      // checks if blogs contains data or not
+      if(blogs.length === 0){
+            return res.json({
+                  status: 404,
+                  message: "No blogs found"
+            })
+      }else{
+            return res.json({
+                  status: 200,
+                  message: "Blogs fetched successfully",
+                  data: blogs
+            })
+      }
+})
+
+// **Get single blog API
+app.get("/blogs/:id", async(req, res)=>{
+      const { id } = req.params;
+      // **Or
+      // const id = req.params.id;
+      
+      // **First way to fetch single blog
+      // // fetch single blog from Blog Model
+      // const blog = await Blog.find({ _id: id })  // find() returns an array
+      
+      // // checks if blog exists or not
+      // if(blog.length === 0){ // if blog is not found
+      //       return res.json({
+      //             status: 404,
+      //             message: "Blog not found"
+      //       })
+      // }else{
+      //       return res.json({
+      //             status: 200,
+      //             message: "Blog fetched successfully",
+      //             data: blog
+      //       })
+      // }
+
+      // **Alternative way to fetch single blog
+      const blog = await Blog.findById(id) // findById() returns a single object
+      if(blog){
+            return res.json({
+                  status: 200,
+                  message: "Blog fetched successfully",
+                  data: blog
+            })
+      }else{
+            return res.json({
+                  status: 404,
+                  message: "Blog not found"
+            })
+      }
+})
 
 const port = process.env.PORT || 2000
 app.listen(port,()=>{
